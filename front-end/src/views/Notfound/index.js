d1e358e936ffbef46262e9a193c90cb939d04d77
XandaoTP <alexandre8603@gmail.com>
2022-04-13 19:18:17 -0300

    ajuste

--- front-end/src/views/Notfound/index.js
+++ front-end/src/views/Notfound/index.js
@@ -1,9 +1,11 @@
 import { Layout } from "../../components/Layout";
 
 export function NotFoundView () {
+  return (
     <>
     <Layout>
       <h1>Erro 404</h1>
     </Layout>
     </>
-  }
+  )
+}
