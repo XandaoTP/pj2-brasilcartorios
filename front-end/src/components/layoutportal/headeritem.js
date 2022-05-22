5a61a6668465008c9371a250ebaa9bee1e16be85
XandaoTP <alexandre8603@gmail.com>
2022-05-22 18:14:34 -0300

    finaliza

--- front-end/src/components/layoutportal/headeritem.js
+++ front-end/src/components/layoutportal/headeritem.js
@@ -3,7 +3,7 @@ import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
 export function HeaderItens({ item }) {
    const resolved = useResolvedPath(item.to)
-   const match = useMatch({ path: resolved.pathname })
+   const match = useMatch({ path: resolved.pathname, end: item.checkAllPath })
     return (
         <Nav.Item>
             <Nav.Link active={match} as={Link} to={item.to}>{item.text}</Nav.Link>
